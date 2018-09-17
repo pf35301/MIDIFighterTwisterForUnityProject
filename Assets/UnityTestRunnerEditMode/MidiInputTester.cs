fdb57dd3ec2386fa0fd5cbcfcf95bf2d9d4770b5
pf35301 <pf35301@gmail.com>
2018-09-17 21:15:22 +0900

    [change] EditMode UnityTestRunner

---
+++ Assets/UnityTestRunnerEditMode/MidiInputTester.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using TwisterForUnity;
+
+namespace TwisterForUnity.Test {
+    public class MidiInputTester {
+
+        private TwisterParams twisterParams;
+
+        const string TwisterPortName = "Midi Fighter Twister";
+
+        [SetUp]
+        public void SetUp() {
+            twisterParams = new TwisterParams(TwisterPortName);
+            Debug.Log("SetUp");
+        }
+    }
+}
+
