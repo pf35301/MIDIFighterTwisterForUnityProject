96d352992d0260f01aab314ee30f740d93db60da
pf35301 <pf35301@gmail.com>
2018-09-17 14:46:46 +0900

    [add] move file

---
+++ Assets/UnityTestRunner/MidiInput/MIDITester.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TwisterForUnity;
+using TwisterForUnity.MidiJack;
+using MidiJack;
+
+namespace TwisterForUnity.Test {
+    public class MIDITester : MonoBehaviour {
+
+        [SerializeField]
+        private TwisterParams TwisterParam;
+
+	    // Use this for initialization
+	    void Start () {
+            TwisterParam.GetInfo();
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+            var data = new MidiMessage(MidiJackEx.DequeueIncomingData());
+
+            if (data.data1 == TwisterParam.HandCameraTransform.x) {
+
+                if (data.data2 == TwisterParam.RightRoll) {
+                    Debug.Log("Right");
+                }
+
+                if (data.data2 == TwisterParam.LeftRoll) {
+                    Debug.Log("Left");
+                }
+                
+            }
+        }
+    }
+}
+
